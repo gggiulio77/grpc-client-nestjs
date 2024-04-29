79c3aa0de87631c5cdc529ae38a9320d749c07e4
gg <gggiulio77@gmail.com>
2024-04-29 02:02:07 -0300

    feat: add docker support

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,8 @@ import { HELLOWORLD_PACKAGE_NAME } from 'grpc-proto-typescript/nestjs';
                     name: HELLOWORLD_PACKAGE_NAME,
                     transport: Transport.GRPC,
                     options: {
-                        url: '0.0.0.0:4000',
+                        // TODO: replace this with env variable
+                        url: 'grpc-server-rust:5000',
                         package: HELLOWORLD_PACKAGE_NAME,
                         protoPath:
                             './node_modules/grpc-proto-typescript/proto/helloworld.proto',
